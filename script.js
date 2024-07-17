47204aa89a59dd9efd7e0c750ed0c9a11674f91e
Rishab-Rawat <rawatrishab66@gmail.com>
2024-07-17 19:38:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+    if (str === '') {
+        return '';
+    }
+
+    const firstSpaceIndex = str.indexOf(' ');
+
+    if (firstSpaceIndex === -1) {
+        return str.trim();
+    }
+
+    return str.slice(0, firstSpaceIndex).trim();
 }
-
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
